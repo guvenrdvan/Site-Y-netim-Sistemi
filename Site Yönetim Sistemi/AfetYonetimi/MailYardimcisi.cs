68f351d35477c881730e4ec3216f3177dd6faf28
Rıdvan <tkl.hatice01@gmail.com>
2024-05-29 01:08:02 +0300

    Add project files.

---
+++ Site Yönetim Sistemi/AfetYonetimi/MailYardimcisi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+using System.Windows.Forms;
+
+namespace Site_Yönetim_Sistemi.AfetYonetimi
+{
+    public static class MailYardimcisi
+    {
+        private static readonly string smtpServer = "mt-air.guzelhosting.com";
+        private static readonly int smtpPort = 587;
+        private static readonly bool enableSsl = true;
+        private static readonly string smtpUsername = "afetbilgi@xyzapps.net";
+        private static readonly string smtpPassword = "afetbilgi2024";
+        private static readonly List<string> toEmails = new List<string> 
+        {
+            "guvnrdvan@gmail.com",
+            "ibrahim.ozturk1502@gmail.com"
+        };
+        public static void Gonder(string subject, string body, bool isBodyHtml = false)
+        {
+            try
+            {
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress(smtpUsername);
+                    foreach(var toEmail in toEmails)
+                    {
+                        mail.To.Add(toEmail);
+                    }
+                    mail.Subject = subject;
+                    mail.Body = body;
+                    mail.IsBodyHtml = isBodyHtml;
+
+                    using (SmtpClient smtp = new SmtpClient(smtpServer, smtpPort))
+                    {
+                        smtp.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                        smtp.EnableSsl = enableSsl;
+                        smtp.Send(mail);
+                        MessageBox.Show("Mail gönderildi");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                // Hata işleme kodu (örneğin, loglama)
+                Console.WriteLine("Mail gönderilirken hata oluştu: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Site Yönetim Sistemi/Guncelle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Site_Yönetim_Sistemi
+{
+    public partial class Guncelle: Form 
+        
+    {
+        uye uye;
+
+        public Guncelle(uye uye)
+        {
+            InitializeComponent();
+            this.uye = uye;
+            formDoldur();
+        }
+
+        void formDoldur()
+        {
+            cmbCinsiyet.Items.Add("Kadın");
+            cmbCinsiyet.Items.Add("Erkek");
+            cmbMedeni.Items.Add("Evli");
+            cmbMedeni.Items.Add("Bekar");
+        }
+
+        private void Guncelle_Load(object sender, EventArgs e) 
+        {
+
+            txtAd.Text = uye.kullanici.ad;
+            txtSoyad.Text = uye.kullanici.soyAd;
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            
+          
+
+        }
+
+       /* private void btnAracEkle_Click(object sender, EventArgs e)
+        {
+            
+
+        }*/
+
+       /* private void btnAracSil_Click(object sender, EventArgs e)
+        {
+           
+                
+        }*/
+
+      /*  private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }*/
+
+        private void btnDaireSakiniEkle_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnDaireSakiniSil_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnDaireSakiniGuncelle_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
